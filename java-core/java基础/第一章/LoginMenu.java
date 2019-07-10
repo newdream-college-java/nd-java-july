2738d9a5b4253a49fa12595bd2edb8278ca4ca0f
18574862156 <7928426602@qq.com>
2019-07-10 23:33:44 +0800

    第一章 作业

---
+++ java-core/java基础/第一章/LoginMenu.java
@@ -0,0 +1,18 @@
+public class LoginMenu{
+	public static void main(String[]args){
+		System.out.println("\n");
+		System.out.println("               欢迎使用我行我素购物管理系统");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("                          1.登陆系统");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("                          2.退出");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("请选择，输入数字：");
+}
+}
---
+++ java-core/java基础/第一章/test5.java
@@ -0,0 +1,19 @@
+public class test5{
+	public static void main(String[]args){
+		System.out.println("*******************************");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("          人事管理系统");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("\n");
+		System.out.println("          1、添加用户");
+		System.out.println("\n");
+		System.out.println("          2、查询用户");
+		System.out.println("\n");
+		System.out.println("          3、退出用户");
+		System.out.println("\n");
+		System.out.println("*******************************");
+}
+}
