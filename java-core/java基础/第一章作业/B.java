c36e1b31550dae6af617630c54d4754a4169c407
HandsomeDing666 <2895546268@qq.com>
2019-07-10 20:41:41 +0800

    第一次作业

---
+++ java-core/java基础/第一章作业/B.java
@@ -0,0 +1,7 @@
+public class B{
+	public static void main(String[] args){
+		System.out.println("\t我的姓名：小样");
+		System.out.println("\t我的性别：男");
+		System.out.println("\t我的年龄：18");
+	}
+}
---
+++ java-core/java基础/第一章作业/E.java
@@ -0,0 +1,10 @@
+public class E{
+	public static void main(String[] args){
+		System.out.println("\t**************************************\n\n");
+		System.out.println("\t\t\t人事管理系统\n\n");
+		System.out.println("\t\t\t1、添加用户");
+		System.out.println("\t\t\t2、查询用户");
+		System.out.println("\t\t\t3、退出系统");
+		System.out.println("\t**************************************");
+		}
+}
