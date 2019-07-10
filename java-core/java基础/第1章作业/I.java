83aa7832a243c29894c7fe7de57c78413c8c3293
xieqiuxing <2994892782@qq.com>
2019-07-11 00:03:47 +0800

    第一章作业

---
+++ java-core/java基础/第1章作业/I.java
@@ -0,0 +1,7 @@
+public class I{
+	public static void main(String[]args){
+		System.out.println("\t\t欢迎使用我行我素购物系统");
+		System.out.println("\t\t\t1.登录系统");
+		System.out.println("\t\t\t2.退出");
+	}
+}
---
+++ java-core/java基础/第1章作业/J.java
@@ -0,0 +1,9 @@
+public class J{
+	public static void main(String[] args){
+		System.out.println("\t\t欢迎使用我行我素购物管理系统");
+		System.out.println("\t\t\t\t1.客户信息管理");
+		System.out.println("\t\t\t\t2.购物结算");
+		System.out.println("\t\t\t\t3.真情回馈");
+		System.out.prinln("\t\t\t\t4.注销");
+	}
+}
