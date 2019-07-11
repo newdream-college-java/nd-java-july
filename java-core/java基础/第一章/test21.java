824923267e2f48dc8bc2fca34a4584bab574bdff
18574862156 <52736944+18574862156@users.noreply.github.com>
2019-07-11 08:32:43 +0800

    Update test21.java

--- java-core/java基础/第一章/test21.java
+++ java-core/java基础/第一章/test21.java
@@ -1,8 +1,8 @@
 public class test21{
 	public static void main(String[]args){
-		System.out.println("\t\t购物清单\t\t");
-		System.out.println("商品名称\t购买数量\t商品单价\t金额");
-		System.out.println("鳄鱼夹克\t   1\t\t1900\t\t1900");
+		System.out.println("\t\t璐墿娓呭崟\t\t");
+		System.out.println("鍟嗗搧鍚嶇О\t璐拱鏁伴噺\t鍟嗗搧鍗曚环\t閲戦");
+		System.out.println("槌勯奔澶瑰厠\t   1\t\t1900\t\t1900");
 		System.out.println("ipodMP4\t\t   2\t\t400\t\t800");
 }
 }
