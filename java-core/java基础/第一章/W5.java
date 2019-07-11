c00b31171878a72989073b9970e80cb84a9d5a3c
1241840987 <1241840987@qq.com>
2019-07-11 08:31:09 +0800

    第一次作业

---
+++ java-core/java基础/第一章/W5.java
@@ -0,0 +1,11 @@
+public class W5{
+	public static void main(String[] args){
+		System.out.println("**************************");
+		System.out.println("          人事管理系统           ");
+		System.out.println("                                        ");
+		System.out.println("           1、添加用户");
+		System.out.println("           2、查询用户");
+		System.out.println("           3、退出系统");
+		System.out.println("**************************");
+}
+}
---
+++ java-core/java基础/第一章/W8_1.java
@@ -0,0 +1,10 @@
+public class W8_1{
+	public static void main(String[] args){
+		System.out.println("欢迎使用我行我素购物管理系统 ");
+		System.out.println("           1、登录系统");
+		System.out.println("           2、退出 ");
+		System.out.println("                      ");
+		System.out.println(" * * * * * * ** * * * * * * * * * * ** * * * *");
+		System.out.println(" 请选择，输入数字：");
+}
+}
