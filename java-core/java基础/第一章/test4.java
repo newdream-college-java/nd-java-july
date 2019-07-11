ee4fae2dd14e0e81709374905b1b7780351eb8e1
18574862156 <52736944+18574862156@users.noreply.github.com>
2019-07-11 08:33:15 +0800

    Update test4.java

--- java-core/java基础/第一章/test4.java
+++ java-core/java基础/第一章/test4.java
@@ -1,6 +1,6 @@
 public class test4{
 	public static void main(String[]args){
-		System.out.println("ÈÕ\tÒ»\t¶ş\tÈı\tËÄ\tÎå\tÁù");
+		System.out.println("æ—¥\tä¸€\täºŒ\tä¸‰\tå››\täº”\tå…­");
 		System.out.println("\t\t\t1\t2\t3\t4");
 		System.out.println("5\t6\t7\t8\t9\t10\t11");
 		System.out.println("12\t13\t14\t15\t16\t17\t18");
