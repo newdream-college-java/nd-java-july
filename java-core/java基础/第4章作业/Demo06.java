f76affa02b6a2dbe39c35f8791b868508ea3efe1
xieqiuxing <2994892782@qq.com>
2019-07-15 19:41:18 +0800

    计算日期日期更改后的代码

--- java-core/java基础/第4章作业/Demo06.java
+++ java-core/java基础/第4章作业/Demo06.java
@@ -10,44 +10,34 @@ public class Demo06{
 		int b=input.nextInt();
 		int c=0,sum=0;
 		switch(a){
-			case 1:
-			c=0;
-			break;
-			case 2:
-			c=31;
-			break;
-			case 3:
-			c=59;
-			break;
-			case 4:
-			c=90;
-			break;
-			case 5:
-			c=120;
-			break;
-			case 6:
-			c=151;
-			break;
-			case 7:
-			c=181;
-			break;
-			case 8:
-			c=212;
-			break;
-			case 9:
-			c=243;
-			break;
-			case 10:
-			c=273;
-			break;
-			case 11:
-			c=303;
-			break;
 			case 12:
-			c=334;
+			c+=30;
+			case 11:
+			c+=31;
+			case 10:
+			c+=30;
+			case 9:
+			c+=31;
+			case 8:
+			c+=31;
+			case 7:
+			c+=30;
+			case 6:
+			c+=31;
+			case 5:
+			c+=30;
+			case 4:
+			c+=31;
+			case 3:
+			c+=28;
+			case 2:
+			c+=31;
+			case 1:
+			c+=0;
 			break;
 			default:
 			System.out.println("输入的月份错误");
+			break;	
 		}
 		if(b<=31&&b>=1){
 			sum=c+b;
